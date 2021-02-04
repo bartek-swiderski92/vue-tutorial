6bac8f27af3d5f727da514aa3373dd3a7f600bf5
Phos23 <phos23@gmail.com>
2021-02-04 19:03:40 +0000

    add initial functionallity

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-    
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><b>Fancy Qyuz App</b></b-nav-item>
+      <b-nav-item disabled>Counter: 4/10</b-nav-item> </b-nav
+    >
+  </div>
 </template>
