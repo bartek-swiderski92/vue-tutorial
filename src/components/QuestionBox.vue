f94eb0c709c411acce90115677b15a28f34d8a9a
Phos23 <phos23@gmail.com>
2021-02-04 21:07:57 +0000

    initied answer logic

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -6,8 +6,15 @@
       </template>
 
       <hr class="my-4" />
-
-      <p>List of answers</p>
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+        >
+          {{ answer }}
+        </b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -21,8 +28,35 @@ export default {
     currentQuestion: Object,
     next: Function,
   },
-  mounted() {
-      console.log(this.currentQuestion)
-  }
+  data() {
+    return {
+      selectedIndex: null,
+    };
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    },
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+      console.log(index);
+    },
+    mounted() {
+      console.log(this.currentQuestion);
+    },
+  },
 };
 </script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.btn {
+  margin: 0 5px;
+}
+</style>
